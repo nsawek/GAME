96a63788ec698af860ddf4cf21bce653ce1581fe
nsawek <snowacoll@hotmail.com>
2018-05-06 18:21:12 +0200

    Dodanie pola gry
    Dodanie pola gry, skrypt działania paletek.

---
+++ PP/Assets/MovePlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+    public float s = 20;
+    public string test = "Vert";
+    void FixedUpdate()
+    {
+    float vite = Input.GetAxisRaw(test);
+    GetComponent<Rigidbody2D>().velocity = new Vector2(0, vite) * s;
+    }
+}
