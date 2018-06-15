39678ded1aa5364c504ebda9c938802a5b1f1a46
nsawek <snowacoll@hotmail.com>
2018-06-15 14:07:58 +0200

    Dodanie systemu liczenia punktów pod odbiciu, jak i usuwania punktów gdy piłka ominie paletkę i uderzy w ścianę. Poprawa kolizji dla piłki teraz nie wchodzi w część paletki.

---
+++ PP/Assets/COINS.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COINS : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            FindObjectOfType<POINTS>().Score += 1;
+        if (other.gameObject.tag == "ONE")
+            FindObjectOfType<POINTS>().Score -= 1;
+    }
+}
--- PP/Assets/Points.cs
+++ PP/Assets/Points.cs
@@ -2,16 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Points : MonoBehaviour {
+public class POINTS : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int Score;
 }
